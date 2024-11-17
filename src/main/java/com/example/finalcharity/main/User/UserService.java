9d27286229b8e78de1065c7f197e3635940f3ae2
MRizk01 <104647849+MRizk01@users.noreply.github.com>
2024-11-17 23:55:37 +0200

    added updateUser and getUser

--- src/main/java/com/example/finalcharity/main/User/UserService.java
+++ src/main/java/com/example/finalcharity/main/User/UserService.java
@@ -4,43 +4,21 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
-
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
-    public User addUser(User user) {
-        return userRepository.save(user);
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
     }
 
-    public boolean deleteUser(Integer id) {
-        if (userRepository.existsById(id)) {
-            userRepository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
-
-    // Additional admin-specific methods
-    public List<User> getSuspendedUsers() {
-        return userRepository.findByActiveFalse();
-    }
-
-    public void suspendUser(Integer id) {
-        userRepository.findById(id).ifPresent(user -> {
-            user.setActive(false);
-            userRepository.save(user);
-        });
-    }
-
-    public void unsuspendUser(Integer id) {
-        userRepository.findById(id).ifPresent(user -> {
-            user.setActive(true);
-            userRepository.save(user);
+    public Optional<User> updateUser(Integer id, User user) {
+        return userRepository.findById(id).map(existingUser -> {
+            existingUser.setName(user.getName());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPhone(user.getPhone());
+            existingUser.setActive(user.isActive());
+            return userRepository.save(existingUser);
         });
     }
 }
