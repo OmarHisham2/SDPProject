e083c4875b913862fcbcef27eaed66151e4ca11d
Mohamed-Sohiel-Sayed <mohamedsohel357@gmail.com>
2024-11-17 23:21:20 +0200

    Add EmailRegistration.java

---
+++ src/main/java/com/example/finalcharity/main/User/EmailRegistration.java
@@ -0,0 +1,11 @@
+package com.example.finalcharity.main.User;
+
+
+import java.util.Map;
+
+public class EmailRegistration implements RegistrationStrategy {
+    @Override
+    public void register(Map<String, String> userData) {
+        // Email registration logic
+    }
+}
