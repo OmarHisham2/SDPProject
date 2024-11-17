0068d5301eada3ce51c1d3786ce821efba83e367
Mohamed-Sohiel-Sayed <mohamedsohel357@gmail.com>
2024-11-17 23:21:31 +0200

    Add PhoneRegistration.java

---
+++ src/main/java/com/example/finalcharity/main/User/PhoneRegistration.java
@@ -0,0 +1,11 @@
+package com.example.finalcharity.main.User;
+
+
+import java.util.Map;
+
+public class PhoneRegistration implements RegistrationStrategy {
+    @Override
+    public void register(Map<String, String> userData) {
+        // Phone registration logic
+    }
+}
