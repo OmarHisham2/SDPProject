4a74a71eababec917e06122c456f02fe6bbc2b2a
MRizk01 <104647849+MRizk01@users.noreply.github.com>
2025-01-18 03:45:43 +0200

    update main.java

--- src/main/java/com/example/finalcharity/main/MainApplication.java
+++ src/main/java/com/example/finalcharity/main/MainApplication.java
@@ -1,13 +1,24 @@
 package com.example.finalcharity.main;
 
+import com.example.finalcharity.main.User.Admin.AdminCLI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    @Autowired
+    private AdminCLI adminCLI;
 
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Start the Admin CLI when the application starts
+        adminCLI.start();
+    }
 }
